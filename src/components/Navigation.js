99763abe52084c3237df7ffdf6411c7e19e826ad
KartuzGupta <kartikay.gupta2002@gmail.com>
2021-10-18 11:43:30 +0530

    Highlighted current page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import style_nav from './Navigation.css'
-
+import { NavLink } from 'react-router-dom';
 export default function Navigation(props) {
     return (
         <div style={style_nav}>
@@ -14,12 +14,12 @@ export default function Navigation(props) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
-                    <Nav.Link href="/">Register</Nav.Link>
-                    <Nav.Link href="/">Projects</Nav.Link>
-                    <Nav.Link href="/">Team</Nav.Link>
-                    <Nav.Link href="/">Contact</Nav.Link>
+                    <NavLink exact={true} to="/"  activeStyle={{color: 'rgb(103 212 223)', borderBottom:'2px solid #589ba2', fontWeight: 500}}>Home</NavLink>
+                    <NavLink to="/leaderboard"  activeStyle={{color: 'rgb(103 212 223)', borderBottom:'2px solid #589ba2', fontWeight: 500}}>Leaderboard</NavLink>
+                    <NavLink to="/" >Register</NavLink>
+                    <NavLink to="/" >Projects</NavLink>
+                    <NavLink to="/" >Team</NavLink>
+                    <NavLink to="/" >Contact</NavLink>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
