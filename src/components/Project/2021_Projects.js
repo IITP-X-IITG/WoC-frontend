feeddebc14525618a00205b9a377c25fdab6ff18
Aditya61101 <adityakumarsanni.2001@gmail.com>
2021-10-25 11:29:22 +0530

    Footer added in leaderboard

--- src/components/Project/2021_Projects.js
+++ src/components/Project/2021_Projects.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 function Project2021(){
-return <h2 className="yr-2021" style={{height:'28vh', alignItems:'center', justifyContent:'center', paddingTop:'50px'}}>Coming Soon</h2>;
+// return <h2 className="yr-2021" style={{height:'100vh', alignItems:'center', justifyContent:'center', paddingTop:'50px'}}>Coming Soon</h2>;
+ return<div className="yr-2021" style={{display:"flex",alignItems:'center', justifyContent:'center',height:"80vh"}}><h2  style={{color:"white",fontSize:"45px"}}>Coming Soon</h2>
+ </div>;
 }
+
 export default Project2021;
