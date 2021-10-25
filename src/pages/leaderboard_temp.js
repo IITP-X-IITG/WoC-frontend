e4cfcc3503782573be4ea8e3969894df012c998e
Aditya61101 <adityakumarsanni.2001@gmail.com>
2021-10-25 10:58:22 +0530

    temp leaderboard added

---
+++ src/pages/leaderboard_temp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Animatedbg from "../components/Animatedbg";
+import NavBar from "../components/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Leaderboardtemp(){
+    return<> 
+     <Animatedbg />
+    <NavBar/>
+    <div style={{display:"flex",alignItems:'center', justifyContent:'center', width:"100vw",height:"100vh"}}><h2  style={{color:"white",fontSize:"50px"}}>Coming Soon</h2>
+    </div>
+     </>;
+  
+}
+export default Leaderboardtemp;
