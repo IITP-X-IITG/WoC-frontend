fd04e4106213e15fb5a170717618bb1e9b07d637
KartuzGupta <kartikay.gupta2002@gmail.com>
2021-10-27 17:50:25 +0530

    Changes in Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,23 +6,32 @@ import {GoLocation} from 'react-icons/go'
 import {BsFacebook} from 'react-icons/bs'
 import { VscGithubInverted } from "react-icons/vsc";
 import footer_style from './footer.css'
-
+import nwocLogo from '../pages/Images/nwocLogo.svg'
+import guwLogo from '../pages/Images/codingclubIITG.png'
 export default function footer(props) {
     return (
         <div className="footer" style={footer_style} >
             <div className="foot" style={{ backgroundColor: props.bg ?? ""}}>
+                <div className="together">
+                    <h1>This time we are together...</h1>
+                    <div className="togetherLogo">
+                        <img src={nwocLogo} alt="" srcset="" />
+                        <p>+</p>
+                        <img src={guwLogo} alt="" srcset="" />
+                    </div>
+                </div>
                 <div className="ql">
                     <h1>Quick Links</h1>
-                    <hr/>
+                    <hr className="hiddenHr" />
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">About Us</Link></li>
+                        <li><Link to="/project">Projects</Link></li>
                         <li><Link to="/conduct">Conduct</Link></li>
                     </ul>
                 </div>
                 <div className="contact">
                     <h1>Contact Us</h1>
-                    <hr/>
+                    <hr className="hiddenHr" />
                     <div className="ico">    
                         <FiSmartphone/>
                         <p>7007222155</p>
@@ -30,6 +39,10 @@ export default function footer(props) {
                     <div className="ico">    
                         <AiOutlineMail/>
                         <p><a href="mailto:njack@iitp.ac.in">njack@iitp.ac.in</a></p>
+                    </div>
+                    <div className="ico">
+                        <AiOutlineMail/>
+                        <p><a href="mailto:codingclubiitg@gmail.com">codingclubiitg@gmail.com</a></p>
                     </div>
                     <div className="ico">    
                         <GoLocation/>
