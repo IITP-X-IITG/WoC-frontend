591c6823fbbab72086921e7696e605a0433f1be5
Aditya61101 <adityakumarsanni.2001@gmail.com>
2021-10-27 21:55:22 +0530

    changes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FiSmartphone} from 'react-icons/fi'
+// import {FiSmartphone} from 'react-icons/fi'
 import {AiOutlineMail, AiFillTwitterCircle} from 'react-icons/ai'
-import {GoLocation} from 'react-icons/go'
-import {BsFacebook} from 'react-icons/bs'
-import { VscGithubInverted } from "react-icons/vsc";
+// import {GoLocation} from 'react-icons/go'
+import {BsFacebook, BsInstagram} from 'react-icons/bs'
+// import { VscGithubInverted } from "react-icons/vsc";
 import footer_style from './footer.css'
 import nwocLogo from '../pages/Images/nwocLogo.svg'
-import guwLogo from '../pages/Images/codingclubIITG.png'
+import guwLogo from '../pages/Images/iitglogo.svg'
 export default function footer(props) {
     return (
         <div className="footer" style={footer_style} >
@@ -32,29 +32,32 @@ export default function footer(props) {
                 <div className="contact">
                     <h1>Contact Us</h1>
                     <hr className="hiddenHr" />
-                    <div className="ico">    
-                        <FiSmartphone/>
-                        <p>7007222155</p>
-                    </div>
+                   
                     <div className="ico">    
                         <AiOutlineMail/>
-                        <p><a href="mailto:njack@iitp.ac.in">njack@iitp.ac.in</a></p>
+                        <p><a href="mailto:njack@iitp.ac.in" target="_blank"  rel="noreferrer">njack@iitp.ac.in</a></p>
                     </div>
                     <div className="ico">
                         <AiOutlineMail/>
-                        <p><a href="mailto:codingclubiitg@gmail.com">codingclubiitg@gmail.com</a></p>
+                        <p><a href="mailto:codingclubiitg@gmail.com" target="_blank"  rel="noreferrer">codingclubiitg@gmail.com</a></p>
                     </div>
-                    <div className="ico">    
-                        <GoLocation/>
-                        <p>IIT Patna</p>
+                    <div className="ico">
+                    <BsFacebook/>
+                        <p><a href="https://www.facebook.com/codingclubiitg/" target="_blank"  rel="noreferrer">IITG CodingClub</a></p>
                     </div>
-                    <div className="icon_box">
-                        <a href="https://github.com/Njack-IITP/" target="_blank" rel="noreferrer"><VscGithubInverted/></a>
-                        <a href="mailto:njack.iitp@gmail.com" target="_blank" rel="noreferrer"><AiOutlineMail/></a>
-                        <a href="https://twitter.com/njackiitp" target="_blank" rel="noreferrer"><AiFillTwitterCircle/></a>
-                        <a href="https://www.facebook.com/njack.iitp/" target="_blank" rel="noreferrer"><BsFacebook/></a>
-                        
+                    <div className="ico">
+                    <BsFacebook/>
+                        <p><a href="https://www.facebook.com/njack.iitp/" target="_blank"  rel="noreferrer">NWOC IITP</a></p>
                     </div>
+                    <div className="ico">
+                    <AiFillTwitterCircle/>
+                        <p><a href="https://twitter.com/njackiitp" target="_blank"  rel="noreferrer">NWOC IITP</a></p>
+                    </div>
+                    <div className="ico">
+                    <BsInstagram/>
+                        <p><a href="https://www.instagram.com/codingclubiitg/" target="_blank"  rel="noreferrer">IITG CodingClub</a></p>
+                    </div>
+                   
                     
                 </div>
             </div>
