499f8e0806d33a0c1bf25f40c2de013aa3cde786
Anurag Deo <anuragdeo03@gmail.com>
2022-12-02 10:45:58 +0530

    Added some 2022 projects

--- src/components/Project/Project_Section.js
+++ src/components/Project/Project_Section.js
@@ -4,6 +4,7 @@ import Project2018 from "./2018_Projects";
 import Project2019_Search from "./2019_Projects";
 import Project2020_Search from "./2020_Projects";
 import Project2021_Search from "./2021_Projects";
+import Project2022_Search from "./2022_Projects";
 
 function Projectsection() {
   const [toggleState, setToggleState] = useState(1);
@@ -20,33 +21,39 @@ function Projectsection() {
           className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(1)}
         >
-          2021
+          2022
         </button>
         <button
           className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(2)}
         >
-          2020
+          2021
         </button>
         <button
           className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(3)}
         >
-          2019
+          2020
         </button>
         <button
           className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(4)}
         >
-          2018
+          2019
         </button>
+        {/* <button
+          className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(4)}
+        >
+          2018
+        </button> */}
       </div>
 
       <div className="content-tabs">
         <div
           className={toggleState === 1 ? "content  active-content" : "content"}
         >
-          <Project2021_Search/>
+          <Project2022_Search/>
        
          
         </div>
@@ -55,20 +62,20 @@ function Projectsection() {
           className={toggleState === 2 ? "content  active-content" : "content"}
         >
           
-          <Project2020_Search/>
+          <Project2021_Search/>
         </div>
 
         <div
           className={toggleState === 3 ? "content  active-content" : "content"}
         >
          
-          <Project2019_Search/>
+          <Project2020_Search/>
         </div>
         <div
           className={toggleState === 4 ? "content  active-content" : "content"}
         >
     
-         <Project2018/>
+         <Project2019_Search/>
         </div>
 
       </div>
