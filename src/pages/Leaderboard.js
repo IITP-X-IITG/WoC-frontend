4bab5d9dc2a9fe5a0f43ff3175494a217bc4637e
addy <addy140603@gmail.com>
2022-12-06 00:11:02 +0530

    fixed minor bugs

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import Animatedbg from "../components/Animatedbg";
 import Navigation from "../components/Navigation";
 import Footer from "../components/footer";
-import Leaderboard from "./Leaderboard/Leaderboard.jsx";
+import Leaderboard from "./leaderboard/Leaderboard.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function LeaderboardMain() {
