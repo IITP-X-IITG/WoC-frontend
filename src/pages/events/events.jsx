e83122599438bbd037736f55216d36f3f9942f32
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2022-12-14 18:51:33 +0530

    new event added

--- src/pages/events/events.jsx
+++ src/pages/events/events.jsx
@@ -5,7 +5,7 @@ import AnimatedBG from "../../components/Animatedbg";
 import "./events.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import FlutterWorkshop from "../Images/FlutterWorkshop.png";
+import { EventsArray } from "./eventsArray";
 
 const Events = () => {
   useEffect(() => {
@@ -22,43 +22,37 @@ const Events = () => {
         <hr />
       </div>
       <div className="event_wrapper">
-        <div className="event_box" data-aos="fade-up" data-aos-once="true">
-          <img
-            src={FlutterWorkshop}
-            alt="FlutterWorkshop"
-            className="eventImg"
-          />
-          <div className="front_ext_event">
-            <div className="event_title">
-              <h1>Flutter Workshop</h1>
-              <a
-                href="https://www.youtube.com/@codepeak8135
-"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-primary btn-apply" type="button">
-                  Our Youtube Channel
-                </button>
-              </a>
+        {EventsArray.map((event) => {
+          return (
+            <div
+              key={event.eventName}
+              className="event_box"
+              data-aos="fade-up"
+              data-aos-once="true"
+            >
+              <img
+                src={event.eventImage.default}
+                alt={event.eventName}
+                className="eventImg"
+              />
+              <div className="front_ext_event">
+                <div className="event_title">
+                  <h1>{event.eventName}</h1>
+                  <a href={event.eventLink} target="_blank" rel="noreferrer">
+                    <button className="btn btn-primary btn-apply" type="button">
+                      {event.eventButton}
+                    </button>
+                  </a>
+                </div>
+                <hr className="exp_hr" />
+                <p>{event.eventDetails}</p>
+              </div>
             </div>
-            <hr className="exp_hr" />
-            <p>
-              CODEPEAK in association with Flutter Kolkata is going to organize
-              an event focusing on the fundamentals of Flutter. Join us live on
-              our youtube channel on December 11th at 2 pm for a special online
-              event with Tanmoy Karmakar as he takes us on a journey to learn
-              the basics of Flutter. This event is perfect for anyone interested
-              in learning the basics of Flutter and how to get started building
-              their own mobile apps. Don't miss this exciting opportunity to
-              learn from an expert and kickstart your journey into the world of
-              flutter development.
-            </p>
-          </div>
-        </div>
+          );
+        })}
       </div>
       <Footer />
     </div>
   );
-}
+};
 export default Events;
---
+++ src/pages/events/eventsArray.js
@@ -0,0 +1,16 @@
+export const EventsArray = [
+    {
+        "eventName":"Flutter Workshop",
+        "eventDetails":"Join us live on our youtube channel on December 11th at 2 pm for a special online event with Tanmoy Karmakar as he takes us on a journey to learn the basics of Flutter. This event is perfect for anyone interested in learning the basics of Flutter and how to get started building their own mobile apps. Don't miss this exciting opportunity to learn from an expert and kickstart your journey into the world of flutter development.",
+        "eventImage":require("../Images/FlutterWorkshop.png"),
+        "eventLink":"https://www.youtube.com/@codepeak8135",
+        "eventButton":"Our Youtube Channel"
+    },
+    {
+        "eventName":"Master Data Structures and Algorithms!",
+        "eventDetails":"A wonderful workshop on Roadmap to Data Structures & Algorithms to master Data structures and Algorithms is presented to you by Suniti from GeeksForGeeks through CODEPEAK in partnership with them. Suniti is an accomplished competitive programmer with great teaching skills. Currently, she is a SWE intern at Atlassian and mentor at GFG. She has solved more than 1200+ problems across all platforms. Her teaching methodology is highly appreciated by the students. This session will help you grasp the basics of Data structure, solve all your DSA-related doubts and help you crack DSA interviews.",
+        "eventImage":require("../Images/GFG Session.png"),
+        "eventLink":"https://bit.ly/3W8bqlJ",
+        "eventButton":"Register Now!"
+    }
+]
