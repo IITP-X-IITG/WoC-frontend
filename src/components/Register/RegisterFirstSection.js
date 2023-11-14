9d5c538c5fa51360505e5e184df8ca2355058cc7
aritroCoder <ogcodinggod@gmail.com>
2023-11-14 14:20:14 +0530

    updated the registration section to be consistent
    Signed-off-by: aritroCoder <ogcodinggod@gmail.com>

--- src/components/Register/RegisterFirstSection.js
+++ src/components/Register/RegisterFirstSection.js
@@ -1,61 +1,58 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Logo from "../../pages/Images/logo_right.png";
-import MentorGuide from "../../Files/MENTORSGUIDE.pdf";
-import StudentGuide from "../../Files/STUDENTGUIDE.pdf";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from '../../pages/Images/logo_right.png';
+import MentorGuide from '../../Files/MENTORSGUIDE.pdf';
+import StudentGuide from '../../Files/STUDENTGUIDE.pdf';
 
 function FirstSection() {
-  return (
-    <>
-      <section className="first-section">
-        <div className="main-div">
-          <div className="flex-item">
-            <div>
-              <h1 className="heading">How it works?</h1>
-              <p className="info">
-                The program is available to all the participants in whichever
-                form they want, both as students or as mentors. To be a mentor,
-                participants will go through a selection process. Selected
-                mentors propose projects, the best of which will be selected for
-                inclusion in the program. Students work on these projects during
-                their winter vacations. Points will be allotted if any student
-                raises or solves any issue. Mentors review pull requests and
-                overall work on your project from various contributors
-                throughout the programming phase of the program. At the end of
-                the coding period, students will be distinguished on the basis
-                of the bestowal they offer.
-                <br />
-                <div className="btnBox">
-                  <div className="d-grid gap-2 col-3 mx-auto">
-                    <a href="https://drive.google.com/file/d/1zgjsiAMsqqExznSTFi1k3lloNOi7LY4O/view" target="_blank" rel="noreferrer">
-                      <button
-                        className="btn btn-primary btn-apply"
-                        type="button"
-                      >
-                        Mentor Guide
-                      </button>
-                    </a>
-                  </div>
-                  <div className="d-grid gap-2 col-3 mx-auto">
-                    <a href={StudentGuide} target="_blank" rel="noreferrer">
-                      <button disabled
-                        className="btn btn-primary btn-apply"
-                        type="button"
-                      >
-                        Student Guide
-                      </button>
-                    </a>
-                  </div>
+    return (
+        <>
+            <section className="first-section">
+                <div className="main-div">
+                    <div className="flex-item">
+                        <div>
+                            <h1 className="heading">How it works?</h1>
+                            <p className="info">
+                                The program is available to all the participants in whichever form they want, both as
+                                students or as mentors. To be a mentor, participants will go through a selection
+                                process. Selected mentors propose projects, the best of which will be selected for
+                                inclusion in the program. Students work on these projects during their winter vacations.
+                                Points will be allotted if any student raises or solves any issue. Mentors review pull
+                                requests and overall work on your project from various contributors throughout the
+                                programming phase of the program. At the end of the coding period, students will be
+                                distinguished on the basis of the bestowal they offer.
+                                <br />
+                                <div className="btnBox">
+                                    <div className="d-grid gap-2 col-3 mx-auto">
+                                        <a
+                                            href="https://drive.google.com/file/d/1zgjsiAMsqqExznSTFi1k3lloNOi7LY4O/view"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <button className="btn btn-primary btn-apply" type="button">
+                                                Mentor Guide
+                                            </button>
+                                        </a>
+                                    </div>
+                                    <div className="d-grid gap-2 col-3 mx-auto">
+                                        <a href="#" rel="noreferrer">
+                                            {' '}
+                                            {/*replace href with {StudentGuide} later and add target="_blank"*/}
+                                            <button disabled className="btn btn-primary btn-apply" type="button">
+                                                Student Guide
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
+                                <br />
+                                To register scroll down.
+                            </p>
+                        </div>
+                        <img src={Logo} alt="logo_right" className="right-logo" />
+                    </div>
                 </div>
-                <br />
-                To register scroll down.
-              </p>
-            </div>
-            <img src={Logo} alt="logo_right" className="right-logo" />
-          </div>
-        </div>
-      </section>
-    </>
-  );
+            </section>
+        </>
+    );
 }
 export default FirstSection;
--- src/components/Register/RegisterSecondSection.js
+++ src/components/Register/RegisterSecondSection.js
@@ -1,89 +1,74 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 function Card() {
+    useEffect(() => {
+        Aos.init({ duration: 800 });
+    }, []);
+    return (
+        <>
+            <section className="second-section">
+                <h1 className="heading2">Registration</h1>
+                <div className="d-flex row justify-content-around">
+                    <div className=" card-cover col-lg-4 col-md-6" data-aos="fade-right" data-aos-once="true">
+                        <div className="h-100 card to-apply-card">
+                            <div className="card-header">
+                                <h4>Participant</h4>
+                            </div>
+                            <div className="card-body">
+                                <div className="Inside-card-div">
+                                    {' '}
+                                    <p className="card-text">
+                                        No matter whether you are a beginner or an expert in open source, we have a
+                                        space for everyone. Register now to become part of one of the largest open
+                                        source events.
+                                    </p>
+                                </div>
+                                <div className="d-grid gap-2 col-6 mx-auto register-button">
+                                    {/*href="https://docs.google.com/forms/d/e/1FAIpQLSf2vbJpg2jtO9lpKLI5LpCkklo3BV_EkpqX3sixNrHh5-u7Vg/viewform"*/}
+                                    <a href="#" rel="noreferrer">
+                                        <button
+                                            className="btn btn-primary btn-apply btn-phone btn-tab my-2"
+                                            type="button"
+                                            disabled="true"
+                                        >
+                                            Coming soon
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-  useEffect(() => {
-    Aos.init({ duration: 800 });
-  }, []);
-  return (
-    <>
-      <section className="second-section">
-        <h1 className="heading2">Registration</h1>
-        <div className="d-flex row justify-content-around">
-          <div
-            className=" card-cover col-lg-4 col-md-6"
-            data-aos="fade-right"
-            data-aos-once="true"
-          >
-            <div className="h-100 card to-apply-card">
-              <div className="card-header">
-                <h4>Participant</h4>
-              </div>
-              <div className="card-body">
-                <div className="Inside-card-div">
-                  {" "}
-                  <p className="card-text">
-                    No matter whether you are a beginner or an expert in open
-                    source, we have a space for everyone. Register now to become
-                    part of one of the largest open source events.
-                  </p>
+                    <div className="card-cover col-lg-4 col-md-12 col-sm-12" data-aos="fade-left" data-aos-once="true">
+                        <div className="h-100 card to-apply-card">
+                            <div className="card-header">
+                                <h4>Mentor</h4>
+                            </div>
+                            <div className="card-body">
+                                <div className="Inside-card-div">
+                                    {' '}
+                                    <p className="card-text">
+                                        Are you a specialist in one or more projects? Then as a mentor, you will have to
+                                        guide a bunch of students and help them to expertise in the field of open source
+                                        contributions.
+                                    </p>
+                                </div>
+                                <div className="d-grid gap-2 col-6 mx-auto">
+                                    <a href="https://forms.gle/6qDeUDr6AVypRixB6" rel="noreferrer" target="blank">
+                                        <button className="btn btn-primary btn-apply my-2" type="button">
+                                            Register
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="d-grid gap-2 col-6 mx-auto register-button">
-                  <a
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSf2vbJpg2jtO9lpKLI5LpCkklo3BV_EkpqX3sixNrHh5-u7Vg/viewform"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <button
-                      className="btn btn-primary btn-apply btn-phone btn-tab my-2"
-                      type="button"
-                    >
-                      Register
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="card-cover col-lg-4 col-md-12 col-sm-12"
-            data-aos="fade-left"
-            data-aos-once="true"
-          >
-            <div className="h-100 card to-apply-card">
-              <div className="card-header">
-                <h4>Mentor</h4>
-              </div>
-              <div className="card-body">
-                <div className="Inside-card-div">
-                  {" "}
-                  <p className="card-text">
-                    Are you a specialist in one or more projects? Then as a
-                    mentor, you will have to guide a bunch of students and help
-                    them to expertise in the field of open source contributions.
-                  </p>
-                </div>
-                <div className="d-grid gap-2 col-6 mx-auto">
-                  <a
-                    href="https://forms.gle/6qDeUDr6AVypRixB6"
-                    rel="noreferrer"
-                    target="blank"
-                  >
-                    <button className="btn btn-primary btn-apply my-2" type="button">
-                      Register
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+            </section>
+        </>
+    );
 }
 export default Card;
