c94b53f6a2d15542699e65dad9a8e9f9dc85edd2
Kushal Agarwal <kushalag02@gmail.com>
2023-11-20 15:12:37 +0530

    new project page added

---
+++ src/components/Project/2023Projects.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import data from "./2023_details.json";
+import dagshub from "../../pages/Images/dagshub.png";
+import clueless from "../../pages/Images/clueless.png";
+import flutterK from "../../pages/Images/flutterK.png";
+
