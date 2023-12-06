78cd10fa14dd0cafa1e8c39c66357d546f3c6852
Kushal Agarwal <kushalag02@gmail.com>
2023-12-06 13:30:33 +0530

    2023 projects displayed

--- src/components/Project/Project_Section.js
+++ src/components/Project/Project_Section.js
@@ -17,32 +17,32 @@ function ProjectSection() {
       <div className="container-project">
         <div className="bloc-tabs">
           <button disabled
-            className={toggleState === 0 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(0)}
-          >
-            2023
-          </button>
-          <button
             className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
             onClick={() => toggleTab(1)}
           >
-            2022
+            2023
           </button>
           <button
             className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
             onClick={() => toggleTab(2)}
           >
-            2021
+            2022
           </button>
           <button
             className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
             onClick={() => toggleTab(3)}
           >
-            2020
+            2021
           </button>
           <button
             className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
             onClick={() => toggleTab(4)}
+          >
+            2020
+          </button>
+          <button
+            className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(5)}
           >
             2019
           </button>
@@ -51,14 +51,14 @@ function ProjectSection() {
         <div className="content-tabs">
           <div
             className={
-              toggleState === 0 ? "content  active-content" : "content"
+              toggleState === 1 ? "content  active-content" : "content"
             }
           >
             <Project2023 />
           </div>
           <div
             className={
-              toggleState === 1 ? "content  active-content" : "content"
+              toggleState === 2 ? "content  active-content" : "content"
             }
           >
             <Project2022 />
@@ -66,7 +66,7 @@ function ProjectSection() {
 
           <div
             className={
-              toggleState === 2 ? "content  active-content" : "content"
+              toggleState === 3 ? "content  active-content" : "content"
             }
           >
             <Project2021 />
@@ -74,14 +74,14 @@ function ProjectSection() {
 
           <div
             className={
-              toggleState === 3 ? "content  active-content" : "content"
+              toggleState === 4 ? "content  active-content" : "content"
             }
           >
             <Project2020 />
           </div>
           <div
             className={
-              toggleState === 4 ? "content  active-content" : "content"
+              toggleState === 5 ? "content  active-content" : "content"
             }
           >
             <Project2019 />
